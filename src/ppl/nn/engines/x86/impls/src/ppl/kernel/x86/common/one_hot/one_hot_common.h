41b6797186e63887dc92555476300849a3da1ec8
czhappy <13611377816@163.com>
2022-07-22 20:35:45 +0800

    do cast and check in indices preprocess

--- src/ppl/nn/engines/x86/impls/src/ppl/kernel/x86/common/one_hot/one_hot_common.h
+++ src/ppl/nn/engines/x86/impls/src/ppl/kernel/x86/common/one_hot/one_hot_common.h
@@ -22,61 +22,84 @@ namespace ppl { namespace kernel { namespace x86 {
 
 template <typename eT>
 static inline ppl::common::RetCode cast2int64_kernel(const eT *src, int64_t* &dst, 
-                                                     const uint64_t length)
-{
-    dst = (int64_t*)malloc(length*sizeof(int64_t));
-    PRAGMA_OMP_PARALLEL_FOR()
+                                                     const uint64_t length, int64_t depth_val) {
+    if(!dst) dst = (int64_t*)malloc(length*sizeof(int64_t));
     for (uint64_t i = 0; i < length; i++) {
-        dst[i] = (int64_t)src[i];
+        int64_t idx = (int64_t)src[i];
+        idx = idx < 0 ? idx + depth_val : idx;
+        if(idx < 0 || idx >= depth_val){
+            return ppl::common::RC_INVALID_VALUE;
+        }
+        dst[i] = idx;
     }
     return ppl::common::RC_SUCCESS;
 }
 
-template <typename eT>
-static inline int64_t cast2int64_kernel(const eT src)
-{
-    return static_cast<int64_t>(src);
-}
-
-static inline int64_t* cast2int64(const ppl::nn::TensorShape *src_shape, const void *src, bool& has_cast)
-{
+static inline int64_t* cast2int64(const ppl::nn::TensorShape *src_shape, const void *src, int64_t depth_val) {
     auto src_type = src_shape->GetDataType();
-    if(src_type == ppl::common::DATATYPE_INT64) return (int64_t*)src;
+    if(src_type == ppl::common::DATATYPE_INT64) return (int64_t*)src; 
     const uint64_t length = src_shape->GetElementsIncludingPadding();
     int64_t* dst = nullptr;
-    has_cast = true;
-    switch (src_type)
-    {
+    ppl::common::RetCode ret;
+    switch (src_type){
     case ppl::common::DATATYPE_FLOAT64:
-        cast2int64_kernel<double>((double*)src, dst, length);
+        ret = cast2int64_kernel<double>((double*)src, dst, length, depth_val);
         break;
     case ppl::common::DATATYPE_FLOAT32:
-        cast2int64_kernel<float>((float*)src, dst, length);
+        ret = cast2int64_kernel<float>((float*)src, dst, length, depth_val);
         break;
     case ppl::common::DATATYPE_INT32:
-        cast2int64_kernel<int32_t>((int32_t*)src, dst, length);
+        ret = cast2int64_kernel<int32_t>((int32_t*)src, dst, length, depth_val);
         break;
     case ppl::common::DATATYPE_INT16:
-        cast2int64_kernel<int16_t>((int16_t*)src, dst, length);
+        ret = cast2int64_kernel<int16_t>((int16_t*)src, dst, length, depth_val);
         break;
     case ppl::common::DATATYPE_INT8:
-        cast2int64_kernel<int8_t>((int8_t*)src, dst, length);
+        ret = cast2int64_kernel<int8_t>((int8_t*)src, dst, length, depth_val);
         break;
     case ppl::common::DATATYPE_UINT32:
-        cast2int64_kernel<uint32_t>((uint32_t*)src, dst, length);
+        ret = cast2int64_kernel<uint32_t>((uint32_t*)src, dst, length, depth_val);
         break;
     case ppl::common::DATATYPE_UINT16:
-        cast2int64_kernel<uint16_t>((uint16_t*)src, dst, length);
+        ret = cast2int64_kernel<uint16_t>((uint16_t*)src, dst, length, depth_val);
         break;
     case ppl::common::DATATYPE_UINT8:
-        cast2int64_kernel<uint8_t>((uint8_t*)src, dst, length);
+        ret = cast2int64_kernel<uint8_t>((uint8_t*)src, dst, length, depth_val);
         break;
     default:
-        has_cast = false;
+        ret = ppl::common::RC_INVALID_VALUE;
         break;
     }
+    if(ret != ppl::common::RC_SUCCESS) return nullptr;
     return dst;
 }
+
+static inline int64_t cast2int64(const ppl::nn::TensorShape *src_shape, const void *src) {
+    auto src_type = src_shape->GetDataType();
+    if(src_type == ppl::common::DATATYPE_INT64) return *((int64_t*)src);
+    switch (src_type){
+    case ppl::common::DATATYPE_FLOAT64:
+        return static_cast<int64_t>(*(double*)src);
+    case ppl::common::DATATYPE_FLOAT32:
+        return static_cast<int64_t>(*(float*)src);
+    case ppl::common::DATATYPE_INT32:
+        return static_cast<int64_t>(*(int32_t*)src);
+    case ppl::common::DATATYPE_INT16:
+        return static_cast<int64_t>(*(int16_t*)src);
+    case ppl::common::DATATYPE_INT8:
+        return static_cast<int64_t>(*(int8_t*)src);
+    case ppl::common::DATATYPE_UINT32:
+        return static_cast<int64_t>(*(uint32_t*)src);
+    case ppl::common::DATATYPE_UINT16:
+        return static_cast<int64_t>(*(uint16_t*)src);
+    case ppl::common::DATATYPE_UINT8:
+        return static_cast<int64_t>(*(uint8_t*)src);
+    default:
+        break;
+    }
+    return -1;
+}
+
 template <typename eT>
 static ppl::common::RetCode one_hot_ndarray_common(
     const void *indices,
@@ -85,74 +108,42 @@ static ppl::common::RetCode one_hot_ndarray_common(
     const ppl::nn::TensorShape *depth_shape,
     const eT *values,
     eT *dst,
-    const int32_t axis)
-{
-    bool indices_cast = false;
-    bool depth_cast = false;
-    int64_t *real_indices = cast2int64(indices_shape, indices, indices_cast);
-    int64_t *real_depth = cast2int64(depth_shape, depth, depth_cast);
-    if(!real_indices || !real_depth) return ppl::common::RC_INVALID_VALUE;
-    int64_t depth_val = *real_depth;
+    const int32_t axis){
+    int64_t depth_val = cast2int64(depth_shape, depth);
+    if(depth_val == -1) return ppl::common::RC_INVALID_VALUE;
+    int64_t *real_indices = cast2int64(indices_shape, indices, depth_val);
+    if(!real_indices) return ppl::common::RC_INVALID_VALUE;
     uint32_t indices_rank = indices_shape->GetDimCount();
     uint32_t real_axis = axis < 0 ? axis + indices_rank + 1 : axis;
     uint64_t outer_dim = indices_shape->GetElementsToDimensionExcludingPadding(real_axis);
     uint64_t inner_dim = indices_shape->GetElementsFromDimensionExcludingPadding(real_axis);
     uint64_t axis_dim = depth_val;
     uint64_t stride = axis_dim * inner_dim;
-    bool index_valid = true;
-    // PPL_OMP_MAX_THREADS
-    const uint64_t min_block_size = 32;
+    eT on_value = values[1];
+    eT off_value = values[0];
+    const uint64_t l2_cap = (ppl::common::GetCpuCacheL2() == 0) ? 128 : ppl::common::GetCpuCacheL2();
+    uint64_t min_block_size = (l2_cap / depth_val / sizeof(eT)) * 0.25; 
     const uint64_t num_block = min<int64_t>(PPL_OMP_MAX_THREADS(), div_up(outer_dim, min_block_size));
     const uint64_t block_body = outer_dim / num_block;
     const uint64_t block_tail = outer_dim % num_block;
-
     PRAGMA_OMP_PARALLEL_FOR()
     for (uint64_t i = 0; i < num_block; i++) {
         const int64_t block_start = i * block_body + (block_tail > i ? i : block_tail);
         const int64_t block_size = block_body + (block_tail > i ? 1 : 0);
         for(int j=block_start; j<block_start+block_size; j++){
             eT *dst_base = dst + j * axis_dim * inner_dim;
-            std::fill(dst_base, dst_base + stride, values[0]);
+            std::fill(dst_base, dst_base + stride, off_value);
             for (uint64_t k = 0; k < inner_dim; ++k) {
-                int64_t idx = real_indices[j * inner_dim + k];
-                idx = idx < 0 ? idx + depth_val : idx;
-                if(idx < 0 || idx >= depth_val){
-                    index_valid = false;
-                    continue;
-                }
+                int64_t idx = real_indices[j * inner_dim + k]; 
                 eT *p_dst = dst_base + k;
-                p_dst[idx * inner_dim] = values[1];
+                p_dst[idx * inner_dim] = on_value;
             }
         }
     }
-    if(indices_cast)  free(real_indices);
-    if(depth_cast)  free(real_depth);
-    if(!index_valid) return ppl::common::RC_INVALID_VALUE;
+    if(real_indices != indices) free(real_indices);
     return ppl::common::RC_SUCCESS;
 }
 
 }}}; // namespace ppl::kernel::x86
 
 #endif // !__ST_PPL_KERNEL_X86_COMMON_ONE_HOT_ONE_HOT_COMMON_H_
-
-
-
-/*
-
-for (uint64_t i = 0; i < outer_dim; i++) {
-        if(!index_valid) continue;
-        eT *dst_base = dst + i * axis_dim * inner_dim; 
-        std::fill(dst_base, dst_base + stride, values[0]);
-        for (uint64_t k = 0; k < inner_dim; ++k) {
-            int64_t idx = real_indices[i * inner_dim + k];
-            idx = idx < 0 ? idx + depth_val : idx;
-            if(idx < 0 || idx >= depth_val){
-                index_valid = false;
-                continue;
-            }
-            eT *p_dst = dst_base + k;
-            p_dst[idx * inner_dim] = values[1];
-        }
-    }
-
-    */
